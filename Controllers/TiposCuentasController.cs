73f3743c228cc1fcbaa5560dd0706de3975317af
Luis Noriega <91396191+noriegaencinas@users.noreply.github.com>
2024-12-12 16:19:39 -0700

    Add files via upload

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,124 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using ManejoPresupuesto.Servicios;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        //private readonly String connectionString;
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            return View(tiposCuentas);
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            tipoCuenta.UsuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var yaExisteCuenta = await repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+
+            if (yaExisteCuenta)
+            {
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre),
+                    $"El nmobre {tipoCuenta.Nombre} ya existe.");
+                return View(tipoCuenta);
+            }
+
+            await repositorioTiposCuentas.Crear(tipoCuenta);
+
+            return RedirectToAction("Index");  
+        }
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+            if (tipoCuenta == null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Editar(TipoCuenta tipoCuenta) 
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuentaExiste = await repositorioTiposCuentas.ObtenerPorId(tipoCuenta.Id, usuarioId);
+            if (tipoCuentaExiste == null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            await repositorioTiposCuentas.Actualizar(tipoCuenta);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null) {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> BorrarTipoCuenta(int id) 
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            await repositorioTiposCuentas.Borrar(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var idsTipoCuentas = tiposCuentas.Select(x => x.Id).ToArray();
+
+            var idsTiposCuentasNoPerteneceAlUsuario = ids.Except(idsTipoCuentas).ToList();
+
+            if (idsTiposCuentasNoPerteneceAlUsuario.Any())
+            {
+                return Forbid();
+            }
+
+            var tiposCuentasOrdenados = ids.Select((valor, indice) => new TipoCuenta { Id = valor, Orden = indice + 1 }).AsEnumerable();
+            await repositorioTiposCuentas.Ordener(tiposCuentasOrdenados);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,27 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [PrimeraLetraMayuscula(ErrorMessage = "La primera letra debe ser mayuscula")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+    }
+}
+
+/* 
+   [StringLength(maximumLength:50, MinimumLength =3, ErrorMessage = "La longitud del campo {0} debe estar entre {1} y {2}")]
+   [Required(ErrorMessage = "El campo {0} es requerido")]
+   [Display(Name = "Nombre del tipo cuenta")]
+   [Required(ErrorMessage = "El campo {0} es requerido")]
+   [EmailAddress(ErrorMessage = "El campo debe ser un email valido")]
+   [Range(minimum:18, maximum:100, ErrorMessage = "El valor debe estar entre {1} {2}")]
+   [Url(ErrorMessage = "El campo debe ser una URL valida")]
+   [CreditCard(ErrorMessage = "La tarjeta de credito no es valida")]
+ */
